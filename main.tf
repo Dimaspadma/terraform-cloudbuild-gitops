fdc452fe39707c8dbac12f84c788dc768fdf8889
Dimaspadma <Dimaspadma@users.noreply.github.com>
2023-04-18 02:25:12 +0700

    Add vpc network

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  cloud {
+    organization = "bangkit"
+
+    workspaces {
+      name = "learn-terraform-github-actions"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.gcp_project
+  region      = var.gcp_region
+  zone        = var.gcp_zone
+  credentials = var.gcp_credential
+}
+
+# Create VPC
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-custom-mode-network"
+  auto_create_subnetworks = false
+  mtu                     = 1460
+}
+
+# resource "random_id" "bucket_prefix" {
+#   byte_length = 8
+# }
+
+# resource "google_storage_bucket" "default" {
+#   name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+#   force_destroy = false
+#   location      = "US"
+#   storage_class = "STANDARD"
+#   versioning {
+#     enabled = true
+#   }
+# }
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "gcp_credential" { }
+variable "gcp_project" { }
+variable "gcp_region" { 
+  default = "us-central1"
+}
+variable "gcp_zone" { 
+  default = "us-central1-c"
+}
