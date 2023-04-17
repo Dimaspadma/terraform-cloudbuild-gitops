72ad8ba8f90f9e55f7fc774a375ffb84a35f9f57
Dimaspadma <Dimaspadma@users.noreply.github.com>
2023-04-18 02:42:03 +0700

    add bucket

--- main.tf
+++ main.tf
@@ -16,22 +16,22 @@ provider "google" {
 }
 
 # Create VPC
-resource "google_compute_network" "vpc_network" {
-  name                    = "my-network"
-  auto_create_subnetworks = false
-  mtu                     = 1460
+# resource "google_compute_network" "vpc_network" {
+#   name                    = "my-network"
+#   auto_create_subnetworks = false
+#   mtu                     = 1460
+# }
+
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
 }
 
-# resource "random_id" "bucket_prefix" {
-#   byte_length = 8
-# }
-
-# resource "google_storage_bucket" "default" {
-#   name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
-#   force_destroy = false
-#   location      = "US"
-#   storage_class = "STANDARD"
-#   versioning {
-#     enabled = true
-#   }
-# }
+resource "google_storage_bucket" "default" {
+  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+  force_destroy = false
+  location      = "US"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+}
