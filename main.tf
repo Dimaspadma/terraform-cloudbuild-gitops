38314672003c890a2d90b3936a21343adf31ee29
Dimaspadma <Dimaspadma@users.noreply.github.com>
2023-04-18 02:59:09 +0700

    remove bucket

--- main.tf
+++ main.tf
@@ -8,37 +8,9 @@ terraform {
   }
 }
 
-terraform {
-  backend "gcs" {
-   bucket  = "${google_storage_bucket}"
-   prefix  = "terraform/state"
- }
-}
-
 provider "google" {
   project     = var.gcp_project
   region      = var.gcp_region
   zone        = var.gcp_zone
   credentials = var.gcp_credential
-}
-
-# Create VPC
-# resource "google_compute_network" "vpc_network" {
-#   name                    = "my-network"
-#   auto_create_subnetworks = false
-#   mtu                     = 1460
-# }
-
-resource "random_id" "bucket_prefix" {
-  byte_length = 8
-}
-
-resource "google_storage_bucket" "default" {
-  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
-  force_destroy = false
-  location      = "US"
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
-  }
 }
