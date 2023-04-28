60a285617ca5eaf26cff7b8ccbf16c2a215d05e2
Dimaspadma <Dimaspadma@users.noreply.github.com>
2023-04-28 16:45:58 +0700

    Change location cluster and change to 2 node

--- gke.tf
+++ gke.tf
@@ -1,12 +1,12 @@
 resource "google_container_cluster" "primary" {
   name     = "hello-world"
-  location = "${var.gcp_zone}"
+  location = "${var.gcp_region}"
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   remove_default_node_pool = true
-  initial_node_count       = 1
+  initial_node_count       = 2
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
