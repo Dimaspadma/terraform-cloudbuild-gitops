fc6ae255d1e5f10b2c754953bd5bf9c88fa6623d
Dimaspadma <Dimaspadma@users.noreply.github.com>
2023-04-28 17:51:40 +0700

    remove vpc and gke

--- gke.tf
+++ gke.tf
@@ -1,41 +1,41 @@
-# Use a Zonal Cluster
+# # Use a Zonal Cluster
 
-resource "google_container_cluster" "primary" {
-  name     = "hello-world"
-  location = "${var.gcp_zone}"
+# resource "google_container_cluster" "primary" {
+#   name     = "hello-world"
+#   location = "${var.gcp_zone}"
 
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
-  remove_default_node_pool = true
-  initial_node_count       = 2
+#   # We can't create a cluster with no node pool defined, but we want to only use
+#   # separately managed node pools. So we create the smallest possible default
+#   # node pool and immediately delete it.
+#   remove_default_node_pool = true
+#   initial_node_count       = 2
 
-  network    = google_compute_network.vpc.name
-  subnetwork = google_compute_subnetwork.subnet.name
-}
+#   network    = google_compute_network.vpc.name
+#   subnetwork = google_compute_subnetwork.subnet.name
+# }
 
-# Separately Managed Node Pool
-resource "google_container_node_pool" "primary_nodes" {
-  name       = google_container_cluster.primary.name
-  location   = var.gcp_zone
-  cluster    = google_container_cluster.primary.name
-  node_count = var.gke_num_nodes
+# # Separately Managed Node Pool
+# resource "google_container_node_pool" "primary_nodes" {
+#   name       = google_container_cluster.primary.name
+#   location   = var.gcp_zone
+#   cluster    = google_container_cluster.primary.name
+#   node_count = var.gke_num_nodes
 
-  node_config {
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+#   node_config {
+#     oauth_scopes = [
+#       "https://www.googleapis.com/auth/logging.write",
+#       "https://www.googleapis.com/auth/monitoring",
+#     ]
 
-    labels = {
-      env = var.gcp_project
-    }
+#     labels = {
+#       env = var.gcp_project
+#     }
 
-    # preemptible  = true
-    machine_type = "n1-standard-1"
-    tags         = ["gke-node", "${var.gcp_project}-gke"]
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
-  }
-}
+#     # preemptible  = true
+#     machine_type = "n1-standard-1"
+#     tags         = ["gke-node", "${var.gcp_project}-gke"]
+#     metadata = {
+#       disable-legacy-endpoints = "true"
+#     }
+#   }
+# }
--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,13 @@
-# VPC
-resource "google_compute_network" "vpc" {
-  name                    = "k8s-vpc"
-  auto_create_subnetworks = "false"
-}
+# # VPC
+# resource "google_compute_network" "vpc" {
+#   name                    = "k8s-vpc"
+#   auto_create_subnetworks = "false"
+# }
 
-# Subnet
-resource "google_compute_subnetwork" "subnet" {
-  name          = "k8s-subnet"
-  region        = var.gcp_region
-  network       = google_compute_network.vpc.name
-  ip_cidr_range = "192.168.0.0/24"
-}
+# # Subnet
+# resource "google_compute_subnetwork" "subnet" {
+#   name          = "k8s-subnet"
+#   region        = var.gcp_region
+#   network       = google_compute_network.vpc.name
+#   ip_cidr_range = "192.168.0.0/24"
+# }
